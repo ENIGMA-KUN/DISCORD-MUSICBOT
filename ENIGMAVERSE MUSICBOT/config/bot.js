a1d220b0ace3848995b22969d52e1546d9cf942e
ENIGMAVERSE <enigmaverse7@gmail.com>
2021-09-18 23:19:29 +0530

    MASTER

---
+++ ENIGMAVERSE MUSICBOT/config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODg3MjE4NzA3NzQ1NjI0MDg2.YUA8zw.HWcrVUwy9kyLqIp_lILPCwBkeQw',
+        prefix: '.',
+        activity: 'ENIGMA KUN',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
